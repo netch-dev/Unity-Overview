e89bc56301013a55689ef84c20fe35a99072525b
netch-dev <daijeln361@gmail.com>
2024-07-25 10:27:22 -0700

    Text

--- Assets/Notes/Text.cs
+++ Assets/Notes/Text.cs
@@ -1,10 +1,15 @@
-
+using TMPro;
 public class Text {
-	// - 1. 3D Text
+	// - 1. 3D Text / UI Text
 	// -- Old legacy component
 	// -- Limited options - not recommended for new projects
 
 	// - 2. TextMeshPro
 	// -- New text component
 	// -- Much more options
+
+	// - Ideally you should use TextMeshPro for all text
+
+	public TextMeshPro textMeshPro; // World text
+	public TextMeshProUGUI textMeshProUGUI; // UI text
 }
