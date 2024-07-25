0d68d8d6b0645c3f4ade5ea49a9c19c8c4e2028a
netch-dev <daijeln361@gmail.com>
2024-07-25 10:19:01 -0700

    Text / TextMeshPro

---
+++ Assets/Notes/Text.cs
@@ -0,0 +1,10 @@
+
+public class Text {
+	// - 1. 3D Text
+	// -- Old legacy component
+	// -- Limited options - not recommended for new projects
+
+	// - 2. TextMeshPro
+	// -- New text component
+	// -- Much more options
+}
