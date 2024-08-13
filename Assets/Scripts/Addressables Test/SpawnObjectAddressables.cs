3ea3415f1b3ff3cdbe48d847f438c220eb776e81
netch-dev <daijeln361@gmail.com>
2024-08-13 10:20:28 -0700

    Updated SpawnObjectAddressables to use an AssetReference instead of a string

--- Assets/Scripts/Addressables Test/SpawnObjectAddressables.cs
+++ Assets/Scripts/Addressables Test/SpawnObjectAddressables.cs
@@ -3,9 +3,27 @@ using UnityEngine.AddressableAssets;
 using UnityEngine.ResourceManagement.AsyncOperations;
 
 public class SpawnObjectAddressables : MonoBehaviour {
+	[SerializeField] private AssetReference assetReference;
+
 	private void Update() {
 		if (Input.GetKeyDown(KeyCode.T)) {
-			Addressables.LoadAssetAsync<GameObject>("Assets/Prefabs/Addressables Test/Environment.prefab").Completed +=
+			LoadAddressableViaReference();
+		}
+	}
+
+	private void LoadAddressableViaReference() {
+		assetReference.LoadAssetAsync<GameObject>().Completed += (asyncOperationHandle) => {
+			if (asyncOperationHandle.Status == AsyncOperationStatus.Succeeded) {
+				Instantiate(asyncOperationHandle.Result);
+			} else {
+				Debug.LogError("Failed to load asset");
+			}
+		};
+	}
+
+	// Shouldn't be used in production but it's possible to load an asset via a string path
+	private void LoadAddressableViaString() {
+		Addressables.LoadAssetAsync<GameObject>("Assets/Prefabs/Addressables Test/Environment.prefab").Completed +=
 				(asyncOperationHandle) => {
 					if (asyncOperationHandle.Status == AsyncOperationStatus.Succeeded) {
 						Instantiate(asyncOperationHandle.Result);
@@ -13,7 +31,5 @@ public class SpawnObjectAddressables : MonoBehaviour {
 						Debug.LogError("Failed to load asset");
 					}
 				};
-		}
 	}
-
 }
