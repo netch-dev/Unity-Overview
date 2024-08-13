c3470c690716967519de4d8666d8e9fb517610f0
netch-dev <daijeln361@gmail.com>
2024-08-13 10:17:24 -0700

    Compacted the addressables load method

--- Assets/Scripts/Addressables Test/SpawnObjectAddressables.cs
+++ Assets/Scripts/Addressables Test/SpawnObjectAddressables.cs
@@ -5,18 +5,15 @@ using UnityEngine.ResourceManagement.AsyncOperations;
 public class SpawnObjectAddressables : MonoBehaviour {
 	private void Update() {
 		if (Input.GetKeyDown(KeyCode.T)) {
-			AsyncOperationHandle<GameObject> asyncOperationHandle =
-				Addressables.LoadAssetAsync<GameObject>("Assets/Prefabs/Addressables Test/Environment.prefab");
-
-			asyncOperationHandle.Completed += AsyncOperationHandle_Completed;
+			Addressables.LoadAssetAsync<GameObject>("Assets/Prefabs/Addressables Test/Environment.prefab").Completed +=
+				(asyncOperationHandle) => {
+					if (asyncOperationHandle.Status == AsyncOperationStatus.Succeeded) {
+						Instantiate(asyncOperationHandle.Result);
+					} else {
+						Debug.LogError("Failed to load asset");
+					}
+				};
 		}
 	}
 
-	private void AsyncOperationHandle_Completed(AsyncOperationHandle<GameObject> obj) {
-		if (obj.Status == AsyncOperationStatus.Succeeded) {
-			Instantiate(obj.Result);
-		} else {
-			Debug.LogError("Failed to load asset");
-		}
-	}
 }
