327d88dbcfe050719f2a71d215dbcb4ec934691d
netch-dev <daijeln361@gmail.com>
2024-08-13 10:28:21 -0700

    Updated SpawnObjectAddressables to use label references

--- Assets/Scripts/Addressables Test/SpawnObjectAddressables.cs
+++ Assets/Scripts/Addressables Test/SpawnObjectAddressables.cs
@@ -5,10 +5,22 @@ using UnityEngine.ResourceManagement.AsyncOperations;
 public class SpawnObjectAddressables : MonoBehaviour {
 	[SerializeField] private AssetReference assetReference;
 
+	[SerializeField] private AssetLabelReference assetLabelReference;
+
 	private void Update() {
 		if (Input.GetKeyDown(KeyCode.T)) {
-			LoadAddressableViaReference();
+			LoadAddressableViaLabel();
 		}
+	}
+
+	private void LoadAddressableViaLabel() {
+		Addressables.LoadAssetAsync<GameObject>(assetLabelReference).Completed += (asyncOperationHandle) => {
+			if (asyncOperationHandle.Status == AsyncOperationStatus.Succeeded) {
+				Instantiate(asyncOperationHandle.Result);
+			} else {
+				Debug.LogError("Failed to load asset");
+			}
+		};
 	}
 
 	private void LoadAddressableViaReference() {
